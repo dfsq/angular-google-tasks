c43f46182185bcb45556c529e3e8a5e04f16653a
dfsq <dfsq.dfsq@gmail.com>
2014-07-27 23:57:00 +0300

    New googleApi service started instead of old googleService.

--- app/components/services/googleApi-service.js
+++ app/components/services/googleApi-service.js
@@ -11,11 +11,28 @@ angular.module('components.services.googleApi', []).provider('googleApi', functi
 		config = configObj;
 	};
 	
-	this.$get = function() {
-		return function() {
-			return {
-				load: function() {}
-			};
+	this.$get = ['$q', function($q) {
+		
+		return {
+			/**
+			 * Load client library.
+			 * @returns {promise}
+			 */
+			load: function() {
+				
+				var deferred = $q.defer();
+				
+				window.gapiLoaded = function() {
+					gapi.client.setApiKey(config.apiKey);
+					deferred.resolve();
+				};
+
+				var script = document.createElement('script');
+				script.src = 'https://apis.google.com/js/client.js?onload=gapiLoaded';
+				document.body.appendChild(script);
+				
+				return deferred.promise;
+			}
 		};
-	};
+	}]
 });
