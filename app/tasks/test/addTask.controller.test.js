941d1a635c6951434f07e2dab6b264bee3e64b31
dfsq <dfsq.dfsq@gmail.com>
2014-09-15 00:57:19 +0300

    Add task test. Tests file structure.

---
+++ app/tasks/test/addTask.controller.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('Tasks controller', function() {
+
+	var addTaskController,
+		$q,
+		$scope,
+		$modalInstance = {},
+		routeParams = { id: 'xxx' },
+		taskObject = {},
+		tasksService = {
+			createTask: function() {
+				return $q.when(taskObject);
+			}
+		};
+
+	beforeEach(module('googleTasks'));
+
+	beforeEach(inject(function($injector, $controller, $rootScope) {
+
+		$q = $injector.get('$q');
+		$scope = $rootScope.$new();
+
+		addTaskController = $controller('addTaskController', {
+			$scope: $scope,
+			$routeParams: routeParams,
+			$modalInstance: $modalInstance,
+			tasks: tasksService
+		});
+
+		$scope.$digest();
+
+		spyOn(tasksService, 'createTask').and.callThrough();
+	}));
+
+
+	it('Should call method tasks.createTask on saveTask', function() {
+		$scope.saveTask();
+		expect(tasksService.createTask).toHaveBeenCalled();
+		expect(tasksService.createTask.calls.count()).toBe(1);
+	});
+
+});
