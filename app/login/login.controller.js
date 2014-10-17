c97ad817d045685266180c1656771c5be3829c71
dfsq <dfsq.dfsq@gmail.com>
2014-10-18 00:29:44 +0300

    About project popup.

--- app/login/login.controller.js
+++ app/login/login.controller.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
 
-	function loginController($scope, $location, googleApi, security) {
+	function loginController($scope, $location, googleApi, security, sModal) {
 		$scope.login = function() {
 			googleApi.login().then(function(data) {
 				security.authObject = data;
@@ -10,9 +10,16 @@
 				console.log('not ok', data);
 			});
 		};
+
+		$scope.about = function() {
+			sModal.open({
+				scope: $scope,
+				templateUrl: 'login/about.html'
+			});
+		};
 	}
 
-	loginController.$inject = ['$scope', '$location', 'googleApi', 'security'];
+	loginController.$inject = ['$scope', '$location', 'googleApi', 'security', 'sModal'];
 
 	angular.module('login').controller('loginController', loginController);
 })();
