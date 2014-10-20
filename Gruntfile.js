e10f8528dd36007414047f680fb92d17a9f76035
dfsq <dfsq.dfsq@gmail.com>
2014-10-20 23:48:03 +0300

    Missing dependency grunt dist task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,6 +63,7 @@ module.exports = function(grunt) {
 					'bower_components/angular/angular.min.js',
 					'bower_components/angular-route/angular-route.min.js',
 					'bower_components/angular-animate/angular-animate.min.js',
+					'bower_components/angular-sanitize/angular-sanitize.min.js',
 					'bower_components/angular-bootstrap/ui-bootstrap.min.js',
 					'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js'
 				],
